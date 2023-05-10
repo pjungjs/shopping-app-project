8f79bb51cc5d5fba1774d5752797394666250b52
aardvarkpepper <aardvarkpepper@gmail.com>
2023-05-10 15:46:08 -0400

    Update FE Products.

--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -1,12 +1,32 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import Product from "./Product.jsx";
 
+const API = process.env.REACT_APP_API_URL;
+
 function Products({ cart, setCart, loggedInAs}) {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${API}/products`)
+    .then((response) => setProducts(response.data))
+    .catch((e) => console.warn("catch", e));
+  })
+
   return (
     <div>
       <h1>Welcome to the Products page!</h1>
-      <Product cart={cart} setCart={setCart} loggedInAs={loggedInAs}/>
+      {products.map((product) => {
+        return (
+          <Product 
+          cart={cart}
+          setCart={setCart}
+          loggedInAs={loggedInAs}
+          />
+        )
+      })}
     </div>
   )
 }
-
 export default Products;

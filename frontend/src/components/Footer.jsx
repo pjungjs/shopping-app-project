e4b0ce37aecda23502fb50c770e39d4ec0279166
pjungjs <pjungjs@gmail.com>
2023-05-15 08:44:55 -0400

    updated Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,14 +2,19 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <>
-      <a href="#top">
-        <p>Go to the top</p>
-      </a>
-      <Link to="/about">
-        <p>About</p>
-      </Link>
-    </>
+    <div className="mb-3 bg-white text-center align-items-center">
+      <div>
+        <a href="#top" className="m-2">
+          <p>Go to the top</p>
+        </a>
+        <Link to="/about">
+          <p>About</p>
+        </Link>
+      </div>
+      <div className="text-muted">
+        <p>&copy; Copyright by Pursuit. All rights reserved.</p>
+      </div>
+    </div>
   )
 }
 
